c1e5d15c513da233fe6efe8b9403aa4f7231d198
Georgi Georgiev <gogolino1982@gmail.com>
2023-09-22 17:56:29 +0300

    server made

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('hi');
+const express = require('express');
+const PORT = 5000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hi');
+});
+
+app.listen(PORT, () => console.log(`Server is listeninig on ${PORT}...`));
