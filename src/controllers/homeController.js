24060b811a0e8e2ecf893ec8e205761603a34cc9
Georgi Georgiev <gogolino1982@gmail.com>
2023-09-22 20:46:54 +0300

    add home controller

---
+++ src/controllers/homeController.js
@@ -0,0 +1,11 @@
+// const express = require('express');
+
+// const router = express.Router();
+//Better way
+const router = require('express').Router();
+
+router.get('/', (req, res) => {
+    res.render('index');
+});
+
+module.exports = router;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const homeConfig = require('./controllers/homeController');
+
 const PORT = 5000;
-const handlebars = require('express-handlebars');
-const path = require('path');
+
 const expConfigurator = require('./config/expConfig');
 const hbsConfigurator = require('./config/hbsConfig');
 
@@ -12,8 +13,7 @@ expConfigurator(app);
 hbsConfigurator(app);
 
 //Routes
-app.get('/', (req, res) => {
-    res.render('index');
-});
+app.use(homeConfig);
+
 
 app.listen(PORT, () => console.log(`Server is listeninig on ${PORT}...`));
